316f1d1c87c1366c14822470308a108aa15e72c8
Iain Ballard <iain.ballard@7digital.com>
2012-07-13 09:12:00 +0100

    playing with syntax

---
+++ ifs.erl
@@ -0,0 +1,17 @@
+-module(ifs).
+-compile(export_all).
+
+
+wha(X) ->
+	if 
+		X =:= cat -> "cat";
+		X =/= cat -> "not cat"
+	end.
+	
+more_like_it(X) -> case X of
+	cat -> "cat";
+	_ -> "not cat"
+	end.
+	
+overloads(cat) -> "cat";
+overloads(_) -> "not cat".
