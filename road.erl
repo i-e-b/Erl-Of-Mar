b4c7022f1d7c5ec7a5f613f732dde4ff43d575aa
Iain Ballard <iain.ballard@7digital.com>
2012-07-26 17:52:10 +0100

    road

--- road.erl
+++ road.erl
@@ -10,6 +10,18 @@ parse_map(Bin) when is_binary(Bin) ->
     parse_map(binary_to_list(Bin));
 
 parse_map(Str) when is_list(Str) ->
-    [list_to_integer(X) || X <- string:tokens(Str, "\r\n\t ")].
+    Values = [list_to_integer(X) || X <- string:tokens(Str, "\r\n\t ")],
+    group_vals(Values, []).
 
+group_vals([], Acc) ->
+    lists:reverse(Acc);
+group_vals([A,B,X|Rest], Acc) ->
+    group_vals(Rest, [{A,B,X} | Acc]).
+
+shortest_step({A,B,X}, {{DistA,PathA}, {DistB,PathB}}) ->
+    OptA1 = {DistA + A, [{a,A}|PathA]},
+    OptA2 = {DistB + B + X, [{x,X}, {b,B}|PathB]},
+    OptB1 = {DistB + B, [{b,B}|PathB]},
+    OptB2 = {DistA + A + X, [{x,X}, {a,A}|PathA]},
+    {erlang:min(OptA1, OptA2), erlang:min(OptB1, OptB2)}.
 
