ff8441f1267f0d758800369d0cfc715b2ff1a4f3
Iain Ballard <iain.ballard@7digital.com>
2012-08-06 14:34:14 +0100

    link mon

--- messages/linkmon.erl
+++ messages/linkmon.erl
@@ -5,3 +5,21 @@ fiveSecondExit() ->
     timer:sleep(5000),
     exit(reason).
 
+chain(0) ->
+    receive
+        _ -> ok
+    after 2000 ->
+        exit("chain dies here")
+    end;
+chain(N) ->
+    Pid = spawn(fun() -> chain(N-1) end),
+    link(Pid),
+    receive
+        _ -> ok
+    end.
+
+trap_exit(N) ->
+    process_flag(trap_exit, true),
+    spawn_link(fun() -> chain(N) end),
+    receive X -> X end.
+
