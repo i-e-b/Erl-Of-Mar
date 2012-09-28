73d0d13055a241ba3d956952f47190650fa289d4
Iain Ballard <iain.ballard@7digital.com>
2012-09-28 17:21:45 +0100

    test

--- services/trading/src/trade_fsm.erl
+++ services/trading/src/trade_fsm.erl
@@ -6,8 +6,11 @@
         make_offer/2, retract_offer/2, ready/1, cancel/1]).
 
 % Gen fsm callbacks %
--export([init/1, handle_event/3, handle_sync_event/4,
-        handle_info/3, terminate/3, code_change/4]).
+-export([init/1, handle_event/3, handle_sync_event/4, handle_info/3,
+        terminate/3, code_change/4,
+        %custom states
+        idle/2, idle/3, idle_wait/2, idle_wait/3, negotiate/2,
+        negotiate/3, wait/2, ready/2, ready/3]).
 
 %% Public API %%
 %% User -> fsm funcs %%
@@ -55,7 +58,7 @@ accept_negotiate(OtherPid, OwnPid) ->
 do_offer(OtherPid, Item) ->
     gen_fsm:send_event(OtherPid, {do_offer, Item}).
 
-%: Forward cancellation
+%: Forward ca3, ncellation
 undo_offer(OtherPid, Item) ->
     gen_fsm:send_event(OtherPid, {undo_offer, Item}).
 
